a14dc21c7174294518761bac3340bc8bb88af373
Nhan Nguyen <hoainhaannguyen@gmail.com>
2024-05-16 13:45:17 +0700

    Initial source code

--- src/clean-up.mjs
+++ src/clean-up.mjs
@@ -1,13 +1,7 @@
-import { PrismaConfigs } from './app.config.mjs';
 import { Logger } from './services/logger/logger.mjs';
 
 const cleanUp = async (eventType, eventDetails) => {
   Logger.log('info', `Server is stop from event::${eventType}`, eventDetails);
-
-  if (PrismaConfigs.ENABLE_PRISMA) {
-    await Prisma.disconnect();
-    Logger.log('info', `Prisma connection closed`);
-  }
 
   process.exit();
 };
